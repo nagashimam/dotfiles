a21dcd2032a0913488df273373517998d79f6e86
Masato Nagashima <masato.ngsm@gmail.com>
2025-02-08 17:56:10 +0900

    LSPの設定を追加

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,13 +1,20 @@
 require("mason").setup()
-local util = require('util')
-local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require("mason-lspconfig").setup()
+local util = require("util")
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local servers = {"lua_ls"};
+local servers = { "angularls", "html", "cssls", "biome", "ts_ls" }
 for _, server in ipairs(servers) do
+  lspconfig[server].setup({
+    capabilities = capabilities,
+  })
+end
+
+local servers_with_custom_config = { "lua_ls" }
+for _, server in ipairs(servers_with_custom_config) do
   local config = require("lsp." .. server)
-  lspconfig[server].setup(
-    util.merge_tables(config,{
-      capabilities = capabilities,
-    }))
+  lspconfig[server].setup(util.merge_tables(config, {
+    capabilities = capabilities,
+  }))
 end
