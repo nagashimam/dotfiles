7021bf135609c24e4880b6b23cb93cc981f30e98
nagashimam <masato.ngsm@gmail.com>
2025-12-23 08:39:20 +0000

    no env

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias nv nvim
-    . ~/.config/env.sh
-    set -gx OLLAMA_API_BASE http://localhost:11434
 end
